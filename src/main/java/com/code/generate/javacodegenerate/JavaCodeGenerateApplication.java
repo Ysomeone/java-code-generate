1c4f04b6003d2b09d859aff4f474df0a33fc4299
yuangengdong <2442752231@qq.com>
2019-04-12 17:51:41 +0800

    Initial commit

---
+++ src/main/java/com/code/generate/javacodegenerate/JavaCodeGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.code.generate.javacodegenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeGenerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/generate/javacodegenerate/JavaCodeGenerateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.code.generate.javacodegenerate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaCodeGenerateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
